a2d1788d0ed51c08423a4d8c595763402e2133fd
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-10-20 23:25:29 +0800

    feature(Teachers): teachers full crud + navlink

--- SimonProj/Models/Teacher.cs
+++ SimonProj/Models/Teacher.cs
@@ -4,6 +4,6 @@ public class Teacher
 {
     public int ID { get; set; }
     public string LastName { get; set; }
-    public string FirstMidName { get; set; }
+    public string FirstName { get; set; }
     public DateTime JoinedDate { get; set; }
 }
