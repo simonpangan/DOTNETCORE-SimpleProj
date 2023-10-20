3a441ee83d9906102daed8b9e38f26549e3f0dc2
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-10-20 21:12:22 +0800

    feature(Student): Add model

---
+++ SimonProj/Models/Teacher.cs
@@ -0,0 +1,9 @@
+namespace SimonProj.Models;
+
+public class Teacher
+{
+    public int ID { get; set; }
+    public string LastName { get; set; }
+    public string FirstMidName { get; set; }
+    public DateTime JoinedDate { get; set; }
+}
