daa7add58cae06bf1d50b02d308de1bf133910c7
Simon Pangan <simonjoseph.pangan@gmail.com>
2023-10-21 11:58:54 +0800

    feature(Seeder): Add db seeder

---
+++ SimonProj/Data/Seeder.cs
@@ -0,0 +1,51 @@
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using SimonProj.Models;
+
+namespace SimonProj.Data;
+
+public class Seeder
+{
+    public static void Run()
+    {
+        var options = new DbContextOptionsBuilder<ProjContext>()
+            .UseSqlServer(
+                "Server=localhost;" +
+                "Database=Proj;" +
+                "Trusted_Connection=True;" +
+                "TrustServerCertificate=True;" +
+                "MultipleActiveResultSets=true"
+            ).Options;
+
+        using (var context = new ProjContext(options))
+        {
+            context.Database.EnsureCreated();
+
+            Seeder seed = new Seeder();
+            seed.SeedTeachers(seed, context);
+
+            context.SaveChanges();
+        }
+    }
+
+    private void SeedTeachers(Seeder seed, ProjContext context)
+    {
+        if (context.Teachers.Any())
+        {
+            return;
+        }
+
+        var teachersList = seed.GenerateFakeTeachers(30);
+        context.Teachers.AddRange(teachersList);
+    }
+
+    private List<Teacher> GenerateFakeTeachers(int count = 10)
+    {
+        var faker = new Faker<Teacher>()
+            .RuleFor(t => t.FirstName, f => f.Person.FirstName)
+            .RuleFor(t => t.LastName, f => f.Person.LastName)
+            .RuleFor(t => t.JoinedDate, f => f.Date.Past(5));
+
+        return faker.Generate(count);
+    }
+}
